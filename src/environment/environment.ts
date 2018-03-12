edd3a246d092de01076ec893ae3908bfe761f7c7
Tim Andrus <timothyjames.andrus@gmail.com>
2018-03-12 06:23:29 -0600

    first commit

---
+++ src/environment/environment.ts
@@ -0,0 +1,17 @@
+//you should replace this values with yours
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAAm6erVcjpo-4H_SecED0C-XTX8iz-ZaQ",
+    authDomain: "dental-select-ap-1520783356837.firebaseapp.com",
+    databaseURL: "https://dental-select-ap-1520783356837.firebaseio.com/",
+    projectId: "dental-select-ap-1520783356837",
+    storageBucket: "https://console.cloud.google.com/storage/browser/dental-select-ap-1520783356837.appspot.com/?project=dental-select-ap-1520783356837&authuser=0",
+    messagingSenderId: "303898922125"
+  },
+  google_web_client_id: "1092390853283-i98feg7fb1dlsm92kkcbim62855pepi8.apps.googleusercontent.com",
+  facebook_app_id: 826720427470540,
+  wordpress_url: 'http://wordpress.startapplabs.com/blog/',
+  wordpress_rest_api_url: 'http://wordpress.startapplabs.com/blog/wp-json/wp/v2/'
+};
